69e03539c068cf44775d8386ec94e00a6823153d
Levi Walton <22774739+leftengeist@users.noreply.github.com>
2020-02-03 15:22:49 -0500

    Added Files

---
+++ Prog4/GroundPackage.cs
@@ -0,0 +1,203 @@
+﻿//N5284
+//Program 4
+//April 30, 2019
+//CIS 199-75
+//Program creates and displays multiple packages to be shiped, using a GroundPackage class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    class GroundPackage
+    {
+        private const int MIN_ZIP = 00000;              // Minimum zipcode value
+        private const int MAX_ZIP = 99999;              // Maximum zipcode value
+        private const int DEFAULT_ORIGIN = 40202;       // Default value for the origin zipcode
+        private const int DEFAULT_DESTINATION = 90210;  // Default value for the destination zipcode
+        private const int ZONE_CONVERSION = 10000;      // Get the zone from the zip code
+
+        private const double MIN_VALUE = 0.0;           // Minimum value for length, width, height, and weight
+        private const double DEFAULT_VALUE = 1.0;       // Default value for length, width, height, and weight
+
+        private int _originZip;                         // Original zip backing field
+        private int _destinationZip;                    // Destination zip backing field
+
+        private double _length;                         // Length backing field
+        private double _width;                          // Width backing field
+        private double _height;                         // Height backing field
+        private double _weight;                         // Weight backing field
+
+        // Precondition:  00000 <= OriginZip      <= 99999 
+        //                00000 <= DestinationZip <= 99999 
+        //                  0.0 <= Length
+        //                  0.0 <= Width
+        //                  0.0 <= Height
+        //                  0.0 <= Weight
+        // Postcondition: Ground Package is initialized with set origin zip, destination zip, length, width, height, and weight
+        public GroundPackage(int origin = DEFAULT_ORIGIN, int dest = DEFAULT_DESTINATION, double l = DEFAULT_VALUE, double w = DEFAULT_VALUE, double h = DEFAULT_VALUE, double lbs = DEFAULT_VALUE)
+        {
+            OriginZip = origin;
+            DestinationZip = dest;
+            Length = l;
+            Width = w;
+            Height = h;
+            Weight = lbs;
+        }
+
+        public int OriginZip
+        {
+            // Precondition:  None
+            // Postcondition: Origin zip is returned
+            get
+            {
+                return _originZip;
+            }
+
+            // Precondition:  00000 <= value <= 99999
+            // Postcondition: Origin zip is set
+            set
+            {
+                if (value >= MIN_ZIP && value <= MAX_ZIP)
+                    _originZip = value;
+                else
+                    _originZip = DEFAULT_ORIGIN;
+            }
+        }
+
+        public int DestinationZip
+        {
+            // Precondition:  None
+            // Postcondition: Destination zip is returned
+            get
+            {
+                return _destinationZip;
+            }
+
+            // Precondition:  00000 <= value <= 99999
+            // Postcondition: Destination zip is set
+            set
+            {
+                if (value >= MIN_ZIP && value <= MAX_ZIP)
+                    _destinationZip = value;
+                else
+                    _destinationZip = DEFAULT_DESTINATION;
+            }
+        }
+
+        public double Length
+        {
+            // Precondition:  None
+            // Postcondition: Length is returned
+            get
+            {
+                return _length;
+            }
+
+            // Precondition:  0.0 <= value
+            // Postcondition: Length is set
+            set
+            {
+                if (value >= MIN_VALUE)
+                    _length = value;
+                else
+                    _length = DEFAULT_VALUE;
+            }
+        }
+
+        public double Width
+        {
+            // Precondition:  None
+            // Postcondition: Width is returned
+            get
+            {
+                return _width;
+            }
+
+            // Precondition:  0.0 <= value
+            // Postcondition: Width is set
+            set
+            {
+                if (value >= MIN_VALUE)
+                    _width = value;
+                else
+                    _width = DEFAULT_VALUE;
+            }
+        }
+
+        public double Height
+        {
+            // Precondition:  None
+            // Postcondition: Height is returned
+            get
+            {
+                return _height;
+            }
+
+            // Precondition:  0.0 <= value
+            // Postcondition: Height is set
+            set
+            {
+                if (value >= MIN_VALUE)
+                    _height = value;
+                else
+                    _height = DEFAULT_VALUE;
+            }
+        }
+
+        public double Weight
+        {
+            // Precondition:  None
+            // Postcondition: Weight is returned
+            get
+            {
+                return _weight;
+            }
+
+            // Precondition:  0.0 <= value
+            // Postcondition: Weight is set
+            set
+            {
+                if (value >= MIN_VALUE)
+                    _weight = value;
+                else
+                    _weight = DEFAULT_VALUE;
+            }
+        }
+
+        public int ZoneDistance
+        {
+            // Precondition: Destination and Origin zip codes
+            // Postcondition: Returns the distance between the origin and destination zip codes
+            get
+            {
+                return Math.Abs((DestinationZip/ZONE_CONVERSION)-(OriginZip/ZONE_CONVERSION));
+            }
+        }
+
+        // Precondition:  None
+        // Postcondition: The cost for a package is calculated and returned
+        public double CalcCost()
+        {
+            const double SIZE_COST_FACTOR = 0.25;   // Cost for size
+            const double WEIGHT_COST_FACTOR = 0.45; // Cost for weight
+
+            return SIZE_COST_FACTOR * (Length + Width + Height) + WEIGHT_COST_FACTOR * (ZoneDistance + 1) * (Weight);
+        }
+
+        // Precondition:  None
+        // Postcondition: A string holding package information is formated and returned
+        public override string ToString()
+        {
+            return $"Origin Zip: {OriginZip:D5}" + Environment.NewLine +
+                   $"Destination Zip: {DestinationZip:D5}" + Environment.NewLine +
+                   $"Length: {Length:F1}" + Environment.NewLine +
+                   $"Width: {Width:F1}" + Environment.NewLine +
+                   $"Height: {Height:F1}" + Environment.NewLine +
+                   $"Weight: {Weight:F1}";
+        }
+    }
+}
---
+++ Prog4/Program.cs
@@ -0,0 +1,61 @@
+﻿//N5284
+//Program 4
+//April 30, 2019
+//CIS 199-75
+//Program creates and displays multiple packages to be shiped, using a GroundPackage class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an array of 5 ground packages
+            GroundPackage[] packages = new GroundPackage[5];
+
+            // Add packages to Array
+            packages[0] = new GroundPackage(40208, 50403, 12.5, 5.0, 3.0, 2.5);
+            packages[1] = new GroundPackage(12034, 32423,  3.4, 3.4, 3.4, 3.2);
+            packages[2] = new GroundPackage(04323, 83029, 14.5, 8.3, 5.4, 6.7);
+            packages[3] = new GroundPackage(23093, 80423, 12.6, 4.5, 3.3, 2.6);
+            packages[4] = new GroundPackage(32093, 43048, 10.9, 7.2, 4.6, 2.3);
+
+            DisplayPackages(packages);
+
+            // Change some data values in packages
+            packages[0].Length =  2.5;
+            packages[0].Width  =  4.5;
+            packages[1].Height =  6.2;
+            packages[1].Weight =  4.5;
+            packages[2].Length = 12.3;
+            packages[2].Width  =  5.2;
+            packages[3].Height =  3.1;
+            packages[3].Weight =  4.3;
+            packages[3].Length = 15.0;
+            packages[4].Width  =  4.5;
+            packages[4].Height =  4.3;
+            packages[4].Weight =  3.9;
+
+            DisplayPackages(packages);
+        }
+
+        // Precondition:  None
+        // Postcondition: Display packages to console
+        public static void DisplayPackages(GroundPackage[] packages)
+        {
+            foreach(GroundPackage package in packages)
+            {
+                Console.WriteLine($"{package.ToString()}");
+                Console.WriteLine($"{package.CalcCost():C}");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
